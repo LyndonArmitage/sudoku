f7b45cbf95f6940d9c2ae873ba99d65afd795cb3
Lyndon Armitage <lyndon.armitage@gmail.com>
2015-05-09 00:18:49 +0100

    Added final to declared array in tests

--- src/test/java/com/lyndonarmitage/sudoku/SudokuTest.java
+++ src/test/java/com/lyndonarmitage/sudoku/SudokuTest.java
@@ -32,7 +32,7 @@ public class SudokuTest {
             "005010300";
     // @formatter:on
 
-    private static int[][] testArray = {
+    private static final int[][] testArray = {
             {0, 0, 3, 0, 2, 0, 6, 0, 0},
             {9, 0, 0, 3, 0, 5, 0, 0, 1},
             {0, 0, 1, 8, 0, 6, 4, 0, 0},
